ae9841ae360c3c291088422cb5d9460d2600f4e7
m1nsuppp <m1nsuppp@gmail.com>
2022-06-27 01:37:26 +0900

    MVC Pattern 구조화

---
+++ routes/home/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+const ctrl = require('./home.ctrl');
+
+router.get('/', ctrl.index);
+
+module.exports = router;
