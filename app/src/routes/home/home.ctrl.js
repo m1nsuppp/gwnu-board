e809954e0e271be3ef789241bf3aa415550aced1
m1nsuppp <m1nsuppp@gmail.com>
2022-06-29 18:07:12 +0900

    sql 데이터 가져와서 렌더링 하는 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,25 @@
 "use strict";
 
+const boards = require("../../db/db");
+
 const index = (req, res) => {
-  res.render('home/index');
+  const sql = `select * from notices`;
+
+  boards.query(sql, (err, result) => {
+    if (err) throw err;
+    const notices = [];
+
+    for (let data of result) {
+      notices.push({
+        id: data.id,
+        dept: data.dept,
+        title: data.title,
+        link: data.link,
+      });
+    }
+    res.render('home/index', { notices: notices });    
+  });
+
 };
 
 module.exports = {
